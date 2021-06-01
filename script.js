e015867a3175b0c36cca65803bb898b63394de46
reese_s <icipherfly@gmail.com>
2021-06-01 09:41:57 -0700

    Altered Kitty pic. Frodo-21.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var pressButton = document.getElementById("lolcatButton");
 var showCat = document.getElementById("lolcatImage");
-var whatCat = "https://i.postimg.cc/8kdrh6hL/oops-bug.jpg";
+var whatCat = "https://i.postimg.cc/sDTMtgRn/photo-2021-06-01-09-39-26.jpg";
 
 var catAppears = function(){
    showCat.src= whatCat;
